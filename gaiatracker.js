b5e1b4458e59337ab8e2670f9f9801007c42dce5
chiras <a.keller@biozentrum.uni-wuerzburg.de>
2017-08-10 10:23:31 +0200

    scheduled ping on terminals

---
+++ gaiatracker.js
@@ -0,0 +1,33 @@
+const schedule = require('node-schedule');
+const WebHooks = require('node-webhooks')
+const ping = require('ping');
+
+var hosts = ['132.187.198.12', '132.187.198.56'];
+
+var webHooks = new WebHooks({
+    db: './webHooksDB.json', // json file that store webhook URLs 
+})
+
+var status = {};
+
+var schedulePing = schedule.scheduleJob('1 * * * * *', function() {
+
+console.log(status)
+
+
+hosts.forEach(function(host){
+    ping.sys.probe(host, function(isAlive){
+        var msg = isAlive ? 'host ' + host + ' is alive' : 'host ' + host + ' is dead';
+        
+        if (isAlive != status[host] && typeof status[host] !== "undefined"){
+	        console.log("status changed " + msg);
+	        webHooks.trigger('MBD', {text: msg})
+        }else{
+	        console.log("status UNchanged " + msg);
+	    }
+       	status[host] = isAlive;
+                
+    });
+});
+
+})
