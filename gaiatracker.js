bad0abbfbcb23f1bae1d8cbdc8ae549e287628b0
chiras <a.keller@biozentrum.uni-wuerzburg.de>
2017-08-10 10:29:33 +0200

    all terminal IPs, test pc removed

--- gaiatracker.js
+++ gaiatracker.js
@@ -2,7 +2,7 @@ const schedule = require('node-schedule');
 const WebHooks = require('node-webhooks')
 const ping = require('ping');
 
-var hosts = ['132.187.198.12', '132.187.198.56'];
+var hosts = ['132.187.198.12','132.187.198.13','132.187.198.14','132.187.198.18'];
 
 var webHooks = new WebHooks({
     db: './webHooksDB.json', // json file that store webhook URLs 
